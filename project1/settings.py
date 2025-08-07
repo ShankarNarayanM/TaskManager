0876ea35c592193a4901b16f43ace01ed9c140a9
ShankarNarayanM <127712453+ShankarNarayanM@users.noreply.github.com>
2025-08-07 12:19:26 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8zc)t)_nqajhr*!eg(lpw0zh+g)z_yo61#es9r5xnajjven+8a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React app URL
